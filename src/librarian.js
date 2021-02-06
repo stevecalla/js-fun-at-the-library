c5b53524d82315fbf1c0adff92cadc5b011de628
Steve <callasteven@gmail.com>
2021-02-05 21:41:53 -0700

    Pass most of librarian.js

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,31 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
   }
+
+  greetPatron(name, isMorning) {
+    if (isMorning === true) {
+      return `Good morning, ${name}!`;     
+    } else {
+      return `Hello, ${name}!`;
+    }
+  }
+
+  findBook(requestedBook) {
+    if (requestedBook === "The Fifth Season") {
+      // console.log(this.library.checkOutBook)
+      console.log(this.library.shelves)
+      this.library.shelves.fantasy.splice()
+      console.log(this.library.shelves)
+
+      // this.library.checkOutBook(library, book, genre)
+      return "Yes, we have The Fifth Season";
+    } else {
+      return "Sorry, we do not have Five Brief Lessons in Physics";
+    }
+  }
+
 }
 
 module.exports = Librarian;
